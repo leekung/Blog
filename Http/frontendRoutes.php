e91722b3933d30f836f910c7b5ecb70533378b84
Leekung <grmark@gmail.com>
2016-05-23 08:39:07 +0700

    change blog route

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -5,7 +5,6 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 
 $router->group(['prefix' => config('asgard.blog.config.route-prefix')], function (Router $router) {
-    $locale = LaravelLocalization::setLocale() ?: App::getLocale();
-    $router->get(config('asgard.blog.config.route-posts'), ['as' => $locale . '.blog', 'uses' => 'PublicController@index']);
-    $router->get(config('asgard.blog.config.route-posts-slug'), ['as' => $locale . '.blog.slug', 'uses' => 'PublicController@show']);
+    $router->get(config('asgard.blog.config.route-posts'), ['as' => 'public.blog', 'uses' => 'PublicController@index']);
+    $router->get(config('asgard.blog.config.route-posts-slug'), ['as' => 'public.blog.slug', 'uses' => 'PublicController@show']);
 });
